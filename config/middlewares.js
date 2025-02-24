a6459ba5722f02559d2959d368645b05263339c9
salsabil <salsabil.tarek55@gmail.com>
2025-02-24 14:01:35 +0300

    edit

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,19 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: ["https://your-vercel-app.vercel.app"], // Add your frontend URL
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
