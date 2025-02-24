ece28bccc0b180068ba3c2ff39096be52543846f
salsabil <salsabil.tarek55@gmail.com>
2025-02-24 15:08:15 +0300

    cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,12 +2,42 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
+  "strapi::cors",
   {
     name: "strapi::cors",
     config: {
       enabled: true,
       headers: "*",
-      origin: ["https://your-vercel-app.vercel.app"], // Add your frontend URL
+      origin: ["https://elmentor-nextjs.vercel.app"], // Add your frontend URL
+    },
+  },
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "*.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "*.cloudinary.com",
+          ],
+          "script-src": ["'self'", "https:"],
+          "frame-src": ["'self'", "https:"],
+        },
+      },
+      referrerPolicy: { policy: "no-referrer" }, // Adds a Referrer-Policy header to responses to prevent leaking referrer information
+      xssProtection: { enabled: true, mode: "block" }, // Enables X-XSS-Protection header for preventing XSS attacks
     },
   },
   "strapi::poweredBy",
